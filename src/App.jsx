c79f80ed116998da15e1e0b3a46cb6902ae4df55
mmdmshti <mmdmshti@gmail.com>
2025-08-05 15:47:17 +0330

    *

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 
 import Todos from './components/Todos';
-import Home from './Home'
 
 
 function App() {
   return(
     <div className ="bg-gray-100">
       <Todos />
-      <Todos />
-
-      
-
-</div>
+    </div>
     
   
 
